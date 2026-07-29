dd27d3280d08977b06b03149628d7627f2790f1b
ThxMqninq <197995292+ThxMqninq@users.noreply.github.com>
2026-07-29 20:52:26 +0300

    Revert "Update SharedExplosionEffectSystem.cs"
    This reverts commit 62baf99998875220f9cc516c9e302b7efdbcef7d.

--- Content.Shared/_Funkystation/Explosion/EntitySystems/SharedExplosionEffectSystem.cs
+++ Content.Shared/_Funkystation/Explosion/EntitySystems/SharedExplosionEffectSystem.cs
@@ -33,7 +33,7 @@ public abstract partial class SharedExplosionEffectSystem : EntitySystem
                     var angle = _random.NextAngle();
                     var direction = angle.ToVec().Normalized() * 10;
                     var shrapnel = SpawnNextToOrDrop(effect, ent);
-                    if (Exists(shrapnel) && Resolve<TransformComponent>(shrapnel, ref _component))
+                    if (Exists(shrapnel) && Resolve(shrapnel, ref _component))
                     {
                         _throwing.TryThrow(shrapnel, direction, ent.Comp.ShrapnelSpeed / 10);
                     }
